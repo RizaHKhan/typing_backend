28990daf020eea22adf0fe34dd2b76d2d3123260
khanr <khanriza@gmail.com>
2021-01-23 23:40:41 -0500

    removing cors adjustment

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ import cors from "cors"
 import users from "./routes/users.js"
 // import tests from "./routes/tests"
 
-
 dotenv.config()
 const app = express()
 
@@ -16,15 +15,6 @@ app.use(cookieParser())
 process.env.NODE_ENV !== "prod" && app.use(morgan("dev"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "origin, X-Requested-With, Content-Type, Accept"
-  )
-  next()
-})
 
 // registering routes
 app.use("/api/v1/users", users)
